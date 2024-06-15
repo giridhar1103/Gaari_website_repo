dadc3b66bf5efc0f4fbd99e8309bfb3396ca8a12
giridhar1103 <141910777+giridhar1103@users.noreply.github.com>
2024-06-16 01:01:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+require('dotenv').config(); // Load environment variables from the .env file (optional for Vercel)
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const session = require('express-session');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Session configuration
+app.use(session({
+    secret: 'your-secret-key', // Replace with a strong, random secret
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.set('view engine', 'ejs');
 
